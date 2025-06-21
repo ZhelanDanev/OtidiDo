f96765155c6eecc930b8094abe6e6289ac6bc275
Zhelan Danev <bespoke91@gmail.com>
2025-06-21 12:38:35 +0300

    Fix line endings to LF

--- OtidiDo/accounts/models.py
+++ OtidiDo/accounts/models.py
@@ -1,1 +1,16 @@
-from django.contrib.auth.models import AbstractUserfrom django.db import models# Create your models here.class AppUser(AbstractUser):    is_admin_user = models.BooleanField(default=False)class Traveler(models.Model):    user = models.OneToOneField(AppUser, on_delete=models.CASCADE)    bio = models.TextField(blank=True, null=True)    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)    def __str__(self):        return self.user.username
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class AppUser(AbstractUser):
+    is_admin_user = models.BooleanField(default=False)
+
+
+class Traveler(models.Model):
+    user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
